3c988c16d22904d680874baf41224a2314948395
Coot302 <93241079+Coot302@users.noreply.github.com>
2022-06-12 22:58:02 +0200

    Create MOTDEditor.php

---
+++ src/Coot/MOTDEditor/MOTDEditor.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Coot\MOTDEditor;
+
+use pocketmine\command\Command;
+
+use pocketmine\event\Listener;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\tile\Tile;
+
+use pocketmine\utils\Config;
+
+use pocketmine\utils\TextFormat as tf;
+
+use pocketmine\Server;
+
+use pocketmine\player\Player;
+
+use pocketmine\item\ItemIds;
+
+use pocketmine\item\ItemFactory;
+
+use pocketmine\item\Item;
+
+class MOTDEditor extends PluginBase
+
+  {
+
+    
+
+    public $config;
+
+    
+
+    public function onEnable(): void 
+
+    {
+
+      $this->getServer()->getNetwork()->setName($this->getConfig()->get("MOTD"));
+
+  }
+
+  }
