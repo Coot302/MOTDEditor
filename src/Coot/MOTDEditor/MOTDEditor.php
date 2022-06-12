3d2e246c138aab265f682de68d56c5de4656f125
Coot302 <93241079+Coot302@users.noreply.github.com>
2022-06-12 23:51:24 +0200

    update MOTDEditor.php

--- src/Coot/MOTDEditor/MOTDEditor.php
+++ src/Coot/MOTDEditor/MOTDEditor.php
@@ -3,43 +3,25 @@
 namespace Coot\MOTDEditor;
 
 use pocketmine\command\Command;
-
 use pocketmine\event\Listener;
-
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\tile\Tile;
-
 use pocketmine\utils\Config;
-
 use pocketmine\utils\TextFormat as tf;
-
 use pocketmine\Server;
-
 use pocketmine\player\Player;
-
 use pocketmine\item\ItemIds;
-
 use pocketmine\item\ItemFactory;
-
 use pocketmine\item\Item;
 
 class MOTDEditor extends PluginBase
 
   {
-
     
-
     public $config;
 
-    
-
     public function onEnable(): void 
-
     {
-
       $this->getServer()->getNetwork()->setName($this->getConfig()->get("MOTD"));
-
-  }
-
+    }
   }
